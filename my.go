6c32852d3395bdbe219b2230a6cd268390d72091
fengsh <fengsh998@163.com>
2024-03-25 17:19:29 +0800

    first

---
+++ my.go
@@ -0,0 +1,14 @@
+package mypackage
+
+import (
+	"fmt"
+)
+
+func imageurl() {
+	fmt.Println("get image url")
+}
+
+func Avartar() {
+	// 头像
+	imageurl()
+}
---
+++ subpackage/sub.go
@@ -0,0 +1,11 @@
+package subpackage
+
+import "fmt"
+
+func GetRemote() {
+	fmt.Println("GetRemote is Call")
+}
+
+func res() {
+	fmt.Println("res is Call")
+}
